85c94c918e1076788e55c92191f64ea3a6323116
Elle Faraday <faraday.elle@gmail.com>
2018-07-31 15:55:24 +0000

    automatically backed up by learn

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,8 +1,9 @@
 module SongsHelper
-  def if_artist(args)
-    if args
-      <%= f.hidden_field :artist_id %>
+  def if_artist(song)
+    if song.artist.nil?
+      select_tag "song[artist_id]", options_from_collection_for_select(Artist.all, :id, :name)
+    else
+      f.hidden_field :artist_id
     end
   end
-
 end
